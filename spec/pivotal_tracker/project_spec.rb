0b8a0cc82bbe0f3c1064b7fece49d28f58b750da
Malcolm Locke <malc@wholemeal.co.nz>
2010-03-06 22:15:34 +1300

    Re-arranges project spec contexts

--- spec/pivotal_tracker/project_spec.rb
+++ spec/pivotal_tracker/project_spec.rb
@@ -25,51 +25,42 @@ describe PivotalTracker::Project do
         @headers, fixture('features.xml')
     end
   end
+
   context "project" do
     it "should return a project" do
       project = PivotalTracker::Project.find(1)
       project.should be_kind_of(PivotalTracker::Project)
     end
   end
-  context "current" do
+
+  context "iterations" do
     it "should return the current iteration" do
       @project.current.should be_kind_of(PivotalTracker::Iteration)
     end
-  end
-  context "done" do
     it "should return done iterations" do
       @project.done.should be_kind_of(Array)
     end
-  end
-  context "backlog" do
     it "should return backlog iterations" do
       @project.backlog.should be_kind_of(Array)
     end
   end
+
   context "stories" do
     it "should return all stories" do
       @project.stories.should be_kind_of(Array)
       @project.stories.size.should == 3
     end
-  end
-  context "story" do
     it "should return a story by id" do
       @project.story(1).should be_kind_of(PivotalTracker::Story)
     end
-  end
-  context "bugs" do
     it "should return all bugs" do
       @project.bugs.should be_kind_of(Array)
       @project.bugs.size.should == 1
     end
-  end
-  context "chores" do
     it "should return all chores" do
       @project.chores.should be_kind_of(Array)
       @project.chores.size.should == 1
     end
-  end
-  context "features" do
     it "should return all features" do
       @project.features.should be_kind_of(Array)
       @project.features.size.should == 1
